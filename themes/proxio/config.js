668fcad24165c91a893dfab362e7630fd49aabd9
ロ　イみなみ <70193490+roiminami@users.noreply.github.com>
2025-10-22 12:00:45 +0900

    Update config.js

--- themes/proxio/config.js
+++ themes/proxio/config.js
@@ -270,7 +270,7 @@ const CONFIG = {
   PROXIO_CTA_BUTTON_URL: '/sns',
   PROXIO_CTA_BUTTON_TEXT: 'SNSでつながる',
 
-  PROXIO_POST_REDIRECT_ENABLE: true, // 默認開啟重定向
+  PROXIO_POST_REDIRECT_ENABLE: false, // 默認開啟重定向
   PROXIO_POST_REDIRECT_URL: 'https://www.minami.ac.cn', // 重定向域名
   PROXIO_NEWSLETTER: process.env.NEXT_PUBLIC_THEME_PROXIO_NEWSLETTER || false // 是否开启邮件订阅 请先配置mailchimp功能 https://docs.tangly1024.com/article/notion-next-mailchimp
 }
